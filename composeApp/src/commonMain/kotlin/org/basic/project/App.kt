f35a598934f404b730fbc4c0dcf7ae745f6dd629
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2025-08-26 17:50:10 -0300

    version animada 2

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -4,15 +4,20 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,6 +27,9 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        var name: String by remember { mutableStateOf(value = "") }
+        var lastname: String by remember { mutableStateOf("") }
+
         Column (
             modifier = Modifier
                 .background(color = MaterialTheme.colorScheme.primaryContainer)
@@ -29,15 +37,39 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Hola Mundo!"
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                }
             )
-            Text(
-                text = "Programacion 2"
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(name.isNotEmpty()) {
+                Text(
+                    text = "Version animada $name",
+                    fontSize = 24.sp
+                )
+            }
+
+            Spacer(
+                modifier = Modifier.height(40.dp)
             )
-            Text(
-                text = "Android Studio"
+
+            TextField(
+                value = lastname,
+                onValueChange = {
+                    lastname = it
+                }
             )
+
+            AnimatedVisibility(lastname.isNotEmpty()) {
+                Text(
+                    text = "Vesión animada 2: $lastname",
+                    fontSize = 24.sp
+                )
+            }
         }
     }
 }
